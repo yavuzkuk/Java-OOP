13d4198a9bbd543b4f858d7e82b1cfddf05a77ec
Yavuz Kuk <102716932+yavuzkuk@users.noreply.github.com>
2023-01-30 00:31:20 +0300

    Update main.java

--- Interface/main.java
+++ Interface/main.java
@@ -3,14 +3,7 @@ package Interface;
 public class main {
     public static void main(String[] args)
     {
-        Ogretmen o1 = new Ogretmen("Oguz","Fındık",45);
-        o1.walk();
-
-        Ogrenci st1 = new Ogrenci("Zeynep","Güldehan",37);
-        st1.walk();
-
-
-
+        
 
 
     }
