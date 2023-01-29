8483aeeaa21b9a27a77c398a721755b2eb543702
Yavuz Kuk <102716932+yavuzkuk@users.noreply.github.com>
2023-01-30 00:31:53 +0300

    Update main.java

--- Inheritance/main.java
+++ Inheritance/main.java
@@ -3,30 +3,6 @@ package Inheritance;
 public class main {
     public static void main(String[] args)
     {
-//        cat c1 = new cat("Pamuk",5);
-//
-//        Dog d1 = new Dog("Karabaş",6);
-
-//        System.out.println(d1.age);
-//        System.out.println(c1.getName());
-
-
-
-
-
-//        d1.eat();
-//        c1.eat();
-
-
-
-
-
-//        d1.setName("karabas");
-//        System.out.println(d1.getName());
-//
-//        c1.setName("pamuk");
-//        System.out.println(c1.getName());
-
 
 
 
